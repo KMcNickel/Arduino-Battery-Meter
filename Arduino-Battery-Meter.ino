d2d769314f74b4c8ba6ca93cf8771b1b4a3ec7a6
KMcNickel <37396126+KMcNickel@users.noreply.github.com>
2022-06-28 02:07:24 -0700

    Use different adc library for more control

--- Arduino-Battery-Meter.ino
+++ Arduino-Battery-Meter.ino
@@ -1,6 +1,8 @@
 #include <MultiMap.h>
 #include <can.h>
 #include <mcp2515.h>
+#include <ADC.h>
+#include <ADC_util.h>
 
 //#define PRINT_RAW
 
@@ -15,10 +17,13 @@ uint16_t voltInMap[] = {9820, 10830, 11060, 11120, 11180,
                    11510, 11560, 11620, 11740, 11860,
                    11950, 12070, 12250, 12330, 12430, 12600};
 
-float valDivisor = 0.05825;
+float valDivisor = 0.058;
 uint16_t voltage, soc;
 struct can_frame frame;
 MCP2515 mcp2515(10);
+ADC *adc = new ADC();
+
+int adcPin = A1;
 
 void setup()
 {                
@@ -28,13 +33,20 @@ void setup()
   mcp2515.reset();
   mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
   mcp2515.setNormalMode();
+
+  adc->adc0->setAveraging(32);
+  adc->adc0->setResolution(10);
+  adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_LOW_SPEED);
+  adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::LOW_SPEED);
+
+  delay(500);
   
   digitalWrite(13, HIGH);
 }
 
 void loop()                     
 {
-  uint16_t rawVal = analogRead(1);
+  uint16_t rawVal = adc->adc0->analogRead(adcPin);
   voltage = (uint16_t) (rawVal / valDivisor);
   soc = multiMap(voltage, voltInMap, socOutMap, 21);
 
@@ -49,6 +61,11 @@ void loop()
   Serial.print("Voltage: ");
   Serial.print(voltage);
   Serial.println(" mv");
+
+  if(adc->adc0->fail_flag != ADC_ERROR::CLEAR) {
+    Serial.print("ADC0: "); Serial.println(getStringADCError(adc->adc0->fail_flag));
+    adc->resetError();
+  }
 
   frame.can_id = SOC_PACKET_ADDRESS;
   frame.can_dlc = 2;
